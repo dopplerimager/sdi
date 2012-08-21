fb1e34a5686d66ea597879ee3d8b5b27784125b6
Callum Anderson <callumenator@gmail.com>
2012-08-21 14:40:07 -0800

    Update watchdog paths

--- watchdog/restart_sdi3000_obs.bat
+++ watchdog/restart_sdi3000_obs.bat
@@ -1,6 +1,6 @@
-c:\users\sdi3000\bin\pstools\pskill -t idlde
+c:\users\sdi3000\sdi\bin\pstools\pskill -t idlde
 sleep 5
-c:\users\sdi3000\bin\devcon\i386\i386\devcon restart usb*
+c:\users\sdi3000\sdi\bin\devcon\i386\i386\devcon restart usb*
 start "Observing..." "C:\Program Files\ITT\IDL64\bin\bin.x86\idlde.exe" @Poker_Auto_SDI_Observations.pro
-echo. |time > c:\users\sdi3000\watchdog\last_watchdog_restart_time.txt
+echo. |time > c:\users\sdi3000\sdi\watchdog\last_watchdog_restart_time.txt
 exit
--- watchdog/restart_usb.bat
+++ watchdog/restart_usb.bat
@@ -1,1 +1,1 @@
-c:\users\sdi3000\bin\devcon\i386\i386\devcon restart usb*
+c:\users\sdi3000\sdi\bin\devcon\i386\i386\devcon restart usb*
