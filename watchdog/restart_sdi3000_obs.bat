56c0797dfcf454a80761af1a56bc5975e908dde2
Toolik Instrument <ToolikSDI@sdi.com>
2012-10-04 23:30:33 +0000

    Removed absolute path to IDL

--- watchdog/restart_sdi3000_obs.bat
+++ watchdog/restart_sdi3000_obs.bat
@@ -1,6 +1,6 @@
 c:\users\sdi3000\sdi\bin\pstools\pskill -t idlde
 sleep 5
 c:\users\sdi3000\sdi\bin\devcon\i386\i386\devcon restart usb*
-start "Observing..." "C:\Program Files\ITT\IDL64\bin\bin.x86\idlde.exe" @SDI_Auto_Operation
+start "Observing..." idlde.exe @SDI_Auto_Operation
 echo. |time > c:\users\sdi3000\sdi\watchdog\last_watchdog_restart_time.txt
 exit
