06eead8b9156927fe8e68b0e28ee039b00445927
USAFA Instrument <USAFA_SDI@Gakona>
2013-01-22 20:07:24 +0000

    Misc

--- watchdog/watchdog.bat
+++ watchdog/watchdog.bat
@@ -1,4 +1,4 @@
 rem Remark out the following line to disable the watchdog:
-if exist c:\users\sdi3000\sdi\watchdog\console_crash_file.tmp call c:\users\sdi3000\sdi\watchdog\restart_sdi3000_obs.bat
+rem if exist c:\users\sdi3000\sdi\watchdog\console_crash_file.tmp call c:\users\sdi3000\sdi\watchdog\restart_sdi3000_obs.bat
 echo. |time > c:\users\sdi3000\sdi\watchdog\console_crash_file.tmp
 exit
